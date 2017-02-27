b65a4606c724c454834b2ce34f09a87e3bb21dc0
albno273 <albireo0725.mikumiku@gmail.com>
2017-02-27 16:03:51 +0900

    周りに馴染ませて殺意強めにしました

--- KoebutaSlayer.user.js
+++ KoebutaSlayer.user.js
@@ -1,80 +1,107 @@
 // ==UserScript==
 // @name        KoebutaSlayer
 // @namespace   albno273
-// @description 声優へのクソリプを消します。
+// @description 声優へのクソリプを抹殺します。
 // @include     https://twitter.com/*
-// @version     1.0.0
+// @version     1.1.0
 // @grant       none
-// @require https://code.jquery.com/jquery-3.1.0.min.js
+// @require     https://code.jquery.com/jquery-3.1.0.min.js
 // ==/UserScript==
 
-$(function () {
+$(() => {
 
   // このリストにあるアカウントへのリプライのうち、
   // このリスト「外」のアカウントからのものが非表示になります。
   // 初期状態のリストは適当に女性声優のアカウントを50人弱突っ込んであります。
-  // この他対象にしたいアカウントがいる場合はこの部分に追加してください。
+  // この他に対象にしたいアカウントがいる場合はこの部分に追加してください。
   const whiteList = [
-    'suzaki_aya', 'ibuking_1114', 'maaya_taso', '0812asumikana',
-    'numakura_manami', '38kMarie', 'Yaskiyo_manager', 'kurapimk',
-    'kanekosanndesu', 'AyakaOhashi', 'Lynn_0601_', 'takamori_723',
-    'OnoSaki1126', 'nojomiy', 'akekodao', 'yuumin_uchiyama',
-    'marika_0222', 'Miho_Aaaa', 'osorasan703', 'fuchigami_mai',
-    'han_meg_han', 'Erietty_55', 'RiccaTachibana', 'tanezakiatsumi',
-    'yuuka_aisaka', '0309akari', 'mikakokomatsu', 'shiori_izawa',
-    'TomoyoKurosawa', 'eerie_eery', 'mikami_shiori', 'reimatsuzaki',
-    'shimoda_asami', 'Uesakasumire', 'coloruri', 'yuichupunch',
-    'ErikoMatsui', 'ousakichiyo', 'nanjolno', 'Emiryun',
-    'HiRoMi_ig', 'makomorino', 'yukari_tamura'
+    'suzaki_aya',      'ibuking_1114', 'maaya_taso',      '0812asumikana',
+    'numakura_manami', '38kMarie',     'Yaskiyo_manager', 'kurapimk',
+    'kanekosanndesu',  'AyakaOhashi',  'Lynn_0601_',      'takamori_723',
+    'OnoSaki1126',     'nojomiy',      'akekodao',        'yuumin_uchiyama',
+    'marika_0222',     'Miho_Aaaa',    'osorasan703',     'fuchigami_mai',
+    'han_meg_han',     'Erietty_55',   'RiccaTachibana',  'tanezakiatsumi',
+    'yuuka_aisaka',    '0309akari',    'mikakokomatsu',   'shiori_izawa',
+    'TomoyoKurosawa',  'eerie_eery',   'mikami_shiori',   'reimatsuzaki',
+    'shimoda_asami',   'Uesakasumire', 'coloruri',        'yuichupunch',
+    'ErikoMatsui',     'ousakichiyo',  'nanjolno',        'Emiryun',
+    'HiRoMi_ig',       'makomorino',   'yukari_tamura'
   ];
   
-  // element
-  const remove = $("<button>")
-    .text("【クソリプを非表示にする！】")
-    .css({"color": "Red"})
+  let slayCountBeforeExec = 0; // ツイート抹殺数(起動前)
+  let slayCountAfterExec  = 0; // ツイート抹殺数(起動後)
+
+  // slayer element
+  const slayer = $('<button class=\'koebuta-slayer\'>')
+    .text('【クソリプを抹殺する】')
+    .css({'color': '#8899a6', 'font-size': '12px'})
     .hover(
-      function () {
-        $(this).css({"color": "Maroon"});
+      (ev) => {
+        $(ev.currentTarget).css({'color': 'darkred'});
       },
-      function () {
-        $(this).css({"color": "Red"});
+      (ev) => {
+        $(ev.currentTarget).css({'color': '#8899a6'});
       }
     )
-    .on("click", () => {
-      removeKsrp();
-    });
+    .on('click',
+      () => {
+        slayTweet();
+        recordCount();
+      }
+    );
   
-  // delete function
-  function removeKsrp() {
+  // add element
+  $('.client-and-actions > .metadata').after(slayer);
+  
+  // slay tweet
+  function slayTweet() {
     let tweet = $('.js-stream-tweet:has(\'.tweet-text\')');
     if (tweet.length > 0) {
       tweet.each(function (index, element) {
-        let text = $(element).find('.tweet-text').text();
-        // alert('text: ' + text);
         let from = $(element).attr('data-screen-name');
-        // alert('from: ' + from);
         let to   = $(element).attr('data-mentions').split(/ /);
-        // alert('to: ' + to);
         to.forEach(function (value, index, array) {
           if (whiteList.indexOf(from) == - 1 && whiteList.indexOf(value) >= 0) {
-            // 最終行を消してコメントアウトされた部分をアクティブにすると
+            // コメントアウトされた部分をアクティブにすると
             // 面白い感じになります。
-            /*
-            $(element).find('.tweet-text').text('アバーッ！');
-            $(element).find('.fullname').text('Koebuta Slayer');
-            $(element).find('.username').html('<s> @</s><b>koebutaslayer</b>');
-            $(element).find('.js-action-profile-avatar')
-              .attr('src', 'https://pbs.twimg.com/profile_images/716042850903830528/PLNG3AVA.jpg');
-            $(element).find('.AdaptiveMediaOuterContainer').remove();
-            return true;
-            */
-            $(element).css("display", "none");
+
+            // $(element).find('.tweet-text').text('アバーッ！');
+            // $(element).find('.fullname').text('Koebuta Slayer');
+            // $(element).find('.username').html(' <s>@</s><b>koebutaslayer</b>');
+            // $(element).find('.js-action-profile-avatar')
+            //   .attr('src', 'https://pbs.twimg.com/profile_images/716042850903830528/PLNG3AVA.jpg');
+            // $(element).find('.AdaptiveMediaOuterContainer').remove();
+            $(element).css('display', 'none');
+            slayCountAfterExec++;
           }
         });
       });
     }
   }
   
-  // add element
-  $(".client-and-actions > .metadata").after(remove);
+  // count slayed tweet
+  function recordCount() {
+    $('.koebuta-slayer-counter').remove();
+    const counter = $('<span class=\'koebuta-slayer-counter\'>')
+      .text(slayCountAfterExec + '件抹殺しました')
+      .css({'color': 'darkred', 'font-size': '12px'});
+    $('.koebuta-slayer').after(counter);
+    if(slayCountAfterExec - slayCountBeforeExec != 0) {
+      blinkCounter();
+      slayCountBeforeExec = slayCountAfterExec;
+    }
+    slayCountAfterExec = 0;
+  }
+  
+  // blink counter
+  function blinkCounter() {
+    let blinkCount = 0;
+    const blink = setInterval(() => {
+      $('.koebuta-slayer-counter').fadeOut(150).fadeIn(150);
+      blinkCount++;
+      if (blinkCount >= 5)
+        clearInterval(blink);
+    }, 0);
+  }
+
 });
